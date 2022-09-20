082db48a93bb0986aa0d08dffa1576ad058af2ec
matianhe3 <matianhe3@gmail.com>
2022-09-20 15:31:37 +0800

    update lsp

--- nvim/lua/cmp/lspsignature.lua
+++ nvim/lua/cmp/lspsignature.lua
@@ -7,7 +7,7 @@ local function lspsignature()
 
     bind = true, -- This is mandatory, otherwise border config won't get registered.
                 -- If you want to hook lspsaga or other signature handler, pls set to false
-    doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+    doc_lines = 4, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
                   -- set to 0 if you DO NOT want any API comments be shown
                   -- This setting only take effect in insert mode, it does not affect signature help in normal
                   -- mode, 10 by default
@@ -21,8 +21,8 @@ local function lspsignature()
     -- will set to true when fully tested, set to false will use whichever side has more space
     -- this setting will be helpful if you do not want the PUM and floating win overlap
 
-    floating_window_off_x = -10, -- adjust float windows x position.
-    floating_window_off_y = -2, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
+    floating_window_off_x = 1, -- adjust float windows x position.
+    floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
 
     close_timeout = 4000, -- close floating window after ms when laster parameter is entered
     fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
@@ -34,9 +34,9 @@ local function lspsignature()
       border = "rounded"   -- double, rounded, single, shadow, none
     },
 
-    always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+    always_trigger = true, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
 
-    auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+    auto_close_after = 2, -- autoclose signature float win after x sec, disabled if nil.
     extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
     zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
 
