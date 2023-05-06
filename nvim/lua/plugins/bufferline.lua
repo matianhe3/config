22a0fb45eae9ff1dd2a5a9f86622257d9acb7c09
carch <matianhe3@gmail.com>
2023-05-06 17:41:28 +0800

    update nvim buffer close

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 return function()
 	require("bufferline").setup({})
-	vim.api.nvim_set_keymap("n", "<leader>bc", ":BufferLinePickClose<CR>", {})
+	vim.api.nvim_set_keymap("n", "<leader>bC", ":BufferLinePickClose<CR>", {})
+	vim.api.nvim_set_keymap("n", "<leader>bc", ":bdelete<CR>", {})
 	vim.api.nvim_set_keymap("n", "<leader>bn", ":BufferLineCycleNext<CR>", {})
 	vim.api.nvim_set_keymap("n", "<leader>bp", ":BufferLineCyclePrev<CR>", {})
 	vim.api.nvim_set_keymap("n", "<leader>bs", ":BufferLinePick<CR>", {})
