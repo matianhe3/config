340f8c6dbc675354171203d606d5e77285207274
carch <matianhe3@gmail.com>
2024-06-04 13:58:50 +0800

    update

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -18,13 +18,8 @@ return {
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
+    enabled = true,
     opts = { use_diagnostic_signs = true },
-  },
-
-  -- disable trouble
-  {
-    "folke/trouble.nvim",
-    enabled = true 
   },
 
   -- override nvim-cmp and add cmp-emoji
